a8720a71ac5d711f88c198563aabe6b8d853b657
khuonghoanghuy <khuonghoanghuy@gmail.com>
2025-12-21 09:48:41 +0700

    set check update into my repo

--- source/Main.hx
+++ source/Main.hx
@@ -230,7 +230,7 @@ class Main extends Sprite
 		dateNow = dateNow.replace(" ", "_");
 		dateNow = dateNow.replace(":", "'");
 
-		path = "./crash/" + "PsychEngine_" + dateNow + ".txt";
+		path = "./crash/" + "hmengine_" + dateNow + ".txt";
 
 		for (stackItem in callStack)
 		{
@@ -247,7 +247,7 @@ class Main extends Sprite
 		// remove if you're modding and want the crash log message to contain the link
 		// please remember to actually modify the link for the github page to report the issues to.
 		#if officialBuild
-		errMsg += "\nPlease report this error to the GitHub page: https://github.com/ShadowMario/FNF-PsychEngine";
+		errMsg += "\nPlease report this error to the GitHub page: https://github.com/khuonghoanghuy/hmengine-source";
 		#end
 		errMsg += "\n\n> Crash Handler written by: sqirra-rng";
 
